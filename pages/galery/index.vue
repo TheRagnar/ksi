5c74ec30d0a078cd4c5b35b8076ef3e760d4da94
TheRagnar <pebah@inbox.ru>
2019-03-16 19:01:25 +0600

    add normal datafile

--- pages/galery/index.vue
+++ pages/galery/index.vue
@@ -19,7 +19,7 @@
 							<img v-bind:src="galery.image ? galery.image : '/img/noimage.jpg'" alt="">
 						</router-link>
 						<div class="galery__white">
-							<router-link class="galery__name" to="/galery/1">{{ galery.name }}</router-link>
+							<router-link class="galery__name" v-bind:to="`/galery/${galery.id}`" v-html="galery.name"></router-link>
 							<div class="galery__footer">
 								<div class="galery__city">{{ galery.city }}</div>
 								<div class="galery__comments">
@@ -74,84 +74,47 @@ export default {
 			galerries: [
 				{
 					id: 0,
-					image: "http://mockimage.markello.info/400/600",
-					name: "Строительство дома для многодетной семьи",
-					city: "Павлодар",
-					year: 2012,
-					comment: 20,
-					raiting: 3,
+					image: "http://baurzhan.kz/wp-content/uploads/2018/06/PMNL8854.jpg",
+					name: "Навстречу мечте (2018)",
+					city: "г. Алматы",
+					year: 2018,
+					comment: 0,
+					raiting: 8,
+					photos: ["http://baurzhan.kz/wp-content/uploads/2018/06/PMNL8854.jpg", "http://baurzhan.kz/wp-content/uploads/2018/06/PMNL8157.jpg", "http://baurzhan.kz/wp-content/uploads/2018/06/PMNL7981.jpg", "http://baurzhan.kz/wp-content/uploads/2018/06/PMNL7764.jpg", "http://baurzhan.kz/wp-content/uploads/2018/05/UmTS8mGmnoE.jpg", "http://baurzhan.kz/wp-content/uploads/2018/05/khou9Nndeco.jpg", "http://baurzhan.kz/wp-content/uploads/2018/05/EIE4Pz2uR3o.jpg", "http://baurzhan.kz/wp-content/uploads/2018/04/PMNL7198.jpg", "http://baurzhan.kz/wp-content/uploads/2018/04/PMNL7147.jpg", "http://baurzhan.kz/wp-content/uploads/2018/04/PMNL7051.jpg", "http://baurzhan.kz/wp-content/uploads/2018/04/PMNL6922.jpg", "http://baurzhan.kz/wp-content/uploads/2018/04/PMNL6903.jpg", "http://baurzhan.kz/wp-content/uploads/2018/04/PMNL6860.jpg", "http://baurzhan.kz/wp-content/uploads/2018/04/PMNL6326.jpg"],
+					content: null
 				},
 				{
 					id: 1,
-					image: "http://mockimage.markello.info/400/400/nature",
-					name: "Строительство дома для многодетной семьи",
-					city: "Павлодар",
-					year: 2012,
-					comment: 20,
-					raiting: 3,
+					image: "http://zhakiya.kz/imagine/gallery_photo/IMG_3378.jpg",
+					name: "Урок гигиены в детском доме &quot;Перзент&quot;",
+					city: "г. Алматы",
+					year: 2018,
+					comment: 0,
+					raiting: 8,
+					photos: ["http://zhakiya.kz/imagine/gallery_photo/IMG_3378.jpg", "http://zhakiya.kz/imagine/gallery_photo/IMG_3378.jpg", "http://zhakiya.kz/imagine/gallery_photo/IMG_3391.jpg", "http://zhakiya.kz/imagine/gallery_photo/IMG_3397.jpg", "http://zhakiya.kz/imagine/gallery_photo/IMG_3839.jpg", "http://zhakiya.kz/imagine/gallery_photo/IMG_3874.jpg", "http://zhakiya.kz/imagine/gallery_photo/IMG_3419_1.jpg", "http://zhakiya.kz/imagine/gallery_photo/IMG_3426_1.jpg"],
+					content: "В детском доме семейного типа &laquo;Перзент&raquo; проходит &laquo;Урок гигиены&raquo;"
 				},
-				{
+				{	
 					id: 2,
-					image: "http://mockimage.markello.info/400/400/mems",
-					name: "Строительство дома для многодетной семьи",
-					city: "Павлодар",
-					year: 2012,
-					comment: 20,
-					raiting: 3,
+					image: "http://vlige.kz/upload/images/65479_498572_04.JPG",
+					name: "Универсиада",
+					city: "г. Алматы",
+					year: 2017,
+					comment: 0,
+					raiting: 10,
+					photos: ["http://vlige.kz/upload/images/65479_498572_04.JPG", "http://vlige.kz/upload/images/76670_274777_04.JPG", "http://vlige.kz/upload/images/41022_309519_04.JPG", "http://vlige.kz/upload/images/54211_404991_04.JPG", "http://vlige.kz/upload/images/24188_648339_04.JPG", "http://vlige.kz/upload/images/35776_75901_04.JPG", "http://vlige.kz/upload/images/13491_130134_04.JPG", "http://vlige.kz/upload/images/43576_140688_04.jpg", "http://vlige.kz/upload/images/97464_222252_04.jpg"],
+					content: "Универсиада"
 				},
 				{
 					id: 3,
-					image: "http://mockimage.markello.info/400/400/politika",
-					name: "Строительство дома для многодетной семьи",
-					city: "Павлодар",
-					year: 2012,
-					comment: 20,
-					raiting: 3,
-				},
-				{
-					id: 4,
-					image: "http://mockimage.markello.info/400/400/avatars",
-					name: "Строительство дома для многодетной семьи",
-					city: "Павлодар",
-					year: 2012,
-					comment: 20,
-					raiting: 3,
-				},
-				{
-					id: 5,
-					image: "http://mockimage.markello.info/400/400/forest",
-					name: "Строительство дома для многодетной семьи",
-					city: "Павлодар",
-					year: 2012,
-					comment: 20,
-					raiting: 3,
-				},
-				{
-					id: 6,
-					image: "http://mockimage.markello.info/400/400/mountains",
-					name: "Строительство дома для многодетной семьи",
-					city: "Павлодар",
-					year: 2012,
-					comment: 20,
-					raiting: 3,
-				},
-				{
-					id: 7,
-					image: "http://mockimage.markello.info/400/400/noimage",
-					name: "Строительство дома для многодетной семьи",
-					city: "Павлодар",
-					year: 2012,
-					comment: 20,
-					raiting: 3,
-				},
-				{
-					id: 8,
-					image: "http://mockimage.markello.info/400/400/noimage",
-					name: "Строительство дома для многодетной семьи",
-					city: "Павлодар",
-					year: 2012,
-					comment: 20,
-					raiting: 3,
+					image: "http://zhakiya.kz/imagine/gallery_photo/IMG_3378.jpg",
+					name: "ПРЕМЬЕРА ФИЛЬМА И КНИГИ &quot;АУТИЗМ ПОБЕДИМ&quot;",
+					city: "г. Алматы",
+					year: 2015,
+					comment: 0,
+					raiting: 7,
+					photos: ["http://detdom.kz/i_data/1430383487.jpg", "http://detdom.kz/i_data/1430383642.jpg", "http://detdom.kz/i_data/1430383824.jpg", "http://detdom.kz/i_data/1430383928.jpg", "http://detdom.kz/i_data/1430384040.jpg", "http://detdom.kz/i_data/1430384133.jpg", "http://detdom.kz/i_data/1430384248.jpg", "http://detdom.kz/i_data/1430384365.jpg"],
+					content: "ПРЕМЬЕРА ФИЛЬМА И КНИГИ &quot;АУТИЗМ ПОБЕДИМ&quot;"
 				}
 			]
 		}
--- pages/iniciators/_id.vue
+++ pages/iniciators/_id.vue
@@ -112,6 +112,112 @@ export default {
 					}
 				]
 			},
+			iniciators: [
+				{
+					id: 0,
+					image: "http://mockimage.markello.info/400/600",
+					name: "Клуб добряков",
+					city: "Павлодарская область г. Павлодар",
+					phone: "8 707 128 97 75 ",
+					email: "test@mail.ru",
+					status: "В процессе",
+					name: "Строительство детского сада на 80 мест",
+					position: "Успенка",
+					price: "10 000 000 тг"
+				},
+				{
+					id: 1,
+					image: "http://vlige.kz/upload/images/big/96678_732215_21.jpg",
+					name: "Общественный фонд &laquo;Лига волонтеров&raquo;",
+					city: "г. Алматы, Жарокова 289а",
+					phone: "8 707 128 97 75",
+					email: "volunteers.almaty@gmail.com",
+					status: "Завершено",
+					name: "Посадка деревьев",
+					position: "Ботанический сад города Алматы",
+					price: null
+				},
+				{
+					id: 2,
+					image: "http://volunteer.kz/wp-content/themes/volunteer/images/new-logo-main.png",
+					name: "Координационный Центр Добровольцев &laquo;Команда SOS&raquo;",
+					city: "г. Алматы",
+					phone: "8 701 739 60 02",
+					email: "komandasos@gmail.com",
+					status: "В процессе",
+					name: "Помощь приюту для бездомных животных &laquo;Белый Бим&raquo;",
+					position: "пос. «Мынбаев»",
+					price: null
+				},
+				{
+					id: 3,
+					image: "http://detdom.kz/images/logo.gif",
+					name: "Добровольное общество &laquo;Милосердие&raquo;",
+					city: "г. Алматы",
+					phone: "8 7272 676 646",
+					email: "info@detdom.kz",
+					status: "В процессе",
+					name: "Редкая болезнь у Кан Полины",
+					position: "г. Тараз",
+					price: "12 000 USD - 4 528 320 тг"
+				},
+				{
+					id: 4,
+					image: "http://zhakiya.kz/images/img-hands.png",
+					name: "Благотворительный фонд &laquo;Жакия&raquo;",
+					city: "г. Алматы",
+					phone: "8 7273 555 111",
+					email: "sabirova@zhakiya.kz",
+					status: "Завершено",
+					name: "Программа стоматологической помощи &quot;Подари улыбку&quot;",
+					position: "г. Алматы",
+					price: null
+					},
+				{
+					id: 5,
+					image: "http://baurzhan.kz/wp-content/uploads/2017/11/%D0%BB%D0%BE%D0%B3%D0%BE-%D1%84%D0%BE%D0%BD%D0%B4%D0%B0-.png",
+					name: "Благотворительный фонд &laquo;Бауыржан&raquo;",
+					city: "г. Алматы",
+					phone: "8 7273 151 511",
+					email: "info@baurzhan.kz",
+					status: "Запланировано",
+					name: "АЛТЫН ЖҮРЕК",
+					position: "г.Алматы, ул. Толе би, 12",
+					price: null
+				},
+				{
+					id: 6,
+					image: "http://www.kotodog.kz/img/header-bg.png",
+					name: "Фонд помощи бездомным животным &laquo;КотоПес&raquo;",
+					city: "г. Алматы",
+					phone: "8 707 124 88 04",
+					email: null,
+					status: "Завершено",
+					name: "Весений драйленд &quot;Весенний Драйв 2018!!!&quot;",
+					position: "г.Алматы",
+					price: null
+				},
+				{
+					id: 7,
+					image: "http://mockimage.markello.info/400/400/kazakh",
+					name: "Фонд Добрых Дел Алматы",
+					city: "г. Алматы",
+					phone: "8 701 822 92 22", 
+					email: null,
+					status: "Завершено",
+					name: "ФОНД ДОБРЫХ ДЕЛ АЛМАТЫ",
+					position: "г.Алматы",
+					price: null		
+				},
+				{
+					id: 8,
+					image: "https://redcrescent.kz/wp-content/uploads/2017/11/3F9A9482-min.jpg",
+					name: "Красный полумесяц Казахстана",
+					city: "г. Алматы, ул. Кунаева, 86",
+					phone: "8 7272 910 291",
+					email: "info@redcrescent.kz"
+				}
+			],
 			filters: ["Все", "Новый", "В процессе", "Завершенные"],
 			filter: "Все",
 			projects: [{
@@ -133,6 +239,7 @@ export default {
 	},
 	created: function () {
 		this.projects.length > 0 ? this.projectsHave = true : this.projectsHave = false;
+		this.iniciator = this.iniciators[this.$route.params.id];
 	},
 	methods: {
 		chageFilter: function(filter){
